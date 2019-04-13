50f7ee8cc6847003d2476a9727bf43f77766069e
Dal0xE <d.crrenshaw21@gmail.com>
2019-04-13 14:53:19 -0400

    Engine (hopefully) finished

--- engine.js
+++ engine.js
@@ -65,6 +65,10 @@ engine.register = function(entity) {
     this.entities.push(entity);
 }
 
+engine.unregister = function(entity) {
+    this.entities.splice(this.entities.indexOf(entity), 1);
+}
+
 engine.loop = function() {
     this.entities.forEach(function(entity) {
         entity.eventLoopCallback(this.frame);
@@ -86,6 +90,7 @@ engine.createAsset = function(url) {
     a.src = url;
     a.className = "general-gamepiece";
     a.id = this.generateId();
+    document.getElementById("game_window").appendChild(a);
     return a.id;
 }
 
@@ -107,12 +112,23 @@ function Entity(elemID) {
     this.animFrame = 0;
     this.element = document.getElementById(elemID);
     this.type = "UNDEFINED";
+    this.health = 2;
+    this.shield = 1;
+    this.damageModifier = 0;
+    this.damage = function(amount) {
+        this.health -= amount * shield;
+        if (this.health <= 0) {
+            engine.unregister(this);
+            this.element.destroy();
+        }
+    }
 }
 
 function AttackEntity(x, y, damage) {
     Entity.call(this, "generalElem");
     engine.setPostition(this, x, y);
     this.type = "ATTACK";
+    this.shield = 0; //invulnerable
     this.vicinityCallback = function(entity) {
         if (entity.type == "PLAYER") entity.harm(damage);
     }
@@ -133,6 +149,14 @@ function Sink(id, x, y) {
 function Player(id, x, y) {
     Entity.call(this, id);
     engine.setPosition(this, x, y);
+    this.health = 3;
+    this.damageModifier = 0;
+    this.shield = 1;
+    this.damage = function(amount) {
+        this.health -= amount * shield;
+        lowerH(this.health);
+        if (this.health <= 0) gameOver();
+    }
 }
 
 engine.registerPlayer = function() {
